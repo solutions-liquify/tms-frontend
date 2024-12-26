ed0bf74cbfe2f769dfd8f011ea9507f191530c45
Aakash Barbhaya <aakashbarbhaya@gmail.com>
2024-12-26 17:14:36 +0530

    adding zod validation for contact number

--- src/schemas/party-schema.ts
+++ src/schemas/party-schema.ts
@@ -4,7 +4,11 @@ export const PartySchema = z.object({
   id: z.string().optional().nullable(),
   name: z.string().min(1),
   pointOfContact: z.string().optional().nullable(),
-  contactNumber: z.string().optional().nullable(),
+  contactNumber: z
+    .string()
+    .regex(/^\d{10}$/, { message: 'Number is invalid' })
+    .optional()
+    .nullable(),
   email: z.string().email().optional().nullable(),
   addressLine1: z.string().optional().nullable(),
   addressLine2: z.string().optional().nullable(),
